f6e8d97e2a4ba66845b835cf31f4ae87b322b550
Matt Brooks <mbrooks@sangoma.com>
2020-11-25 15:45:53 +0000

    Updated test with an example mock

--- utests/API/CoreDeviceGQLTest.php
+++ utests/API/CoreDeviceGQLTest.php
@@ -6,6 +6,7 @@ require_once('../api/utests/ApiBaseTestCase.php');
 use FreePBX\modules\Core;
 use Exception;
 use FreePBX\modules\Api\utests\ApiBaseTestCase;
+use GuzzleHttp\Client;
 
 class CoreDeviceGQLTest extends ApiBaseTestCase {
 	protected static $core;
@@ -19,6 +20,7 @@ class CoreDeviceGQLTest extends ApiBaseTestCase {
 
 	public static function tearDownAfterClass() {
 		parent::tearDownAfterClass();
+		self::$freepbx->Core = self::$core;
 		foreach(self::$core->getAllDevicesByType() as $device) {
 			if ($device['id'] >= self::$minTestExtension && $device['id'] <= self::$maxTestExtension) {
 				self::$core->delDevice($device['id']);
@@ -40,7 +42,62 @@ class CoreDeviceGQLTest extends ApiBaseTestCase {
 		);
 		self::$core->addDevice($testExtension, 'pjsip', $settings);
 
-		$response = $this->request("query { 
+		$stubConfig = $this->getMockBuilder(\FreePBX\Core::class)
+			->setMethods(array('getDevice'))
+			->getMock();
+
+		$stubConfig = $this->getMock(\FreePBX\Core::class, array('getDevice'));
+		$stubConfig->method('getDevice')
+			->willReturn(Array(
+				'id' => 7070,
+				'tech' => 'sip',
+				'dial' => 'SIP/7071',
+				'devicetype' => 'fixed',
+				'user' => 7045,
+				'description' => 'Test D70',
+				'emergency_cid' => '',
+				'hint_override' => '',
+				'account' => 1234,
+				'accountcode' => '',
+				'aggregate_mwi' => 'yes',
+				'allow' => '',
+				'avpf' => 'no',
+				'callerid' => 'Test D70 <6969>',
+				'context' => 'from-internal',
+				'defaultuser' => '',
+				'device_state_busy_at' => 0,
+				'disallow' => '',
+				'dtmfmode' => 'rfc4733',
+				'force_rport' => 'yes',
+				'icesupport' => 'no',
+				'match' => '',
+				'max_contacts' => 1,
+				'maximum_expiration' => 7200,
+				'media_encryption' => 'no',
+				'media_encryption_optimistic' => 'no',
+				'media_use_received_transport' => 'no',
+				'minimum_expiration' => 60,
+				'mwi_subscription' => 'auto',
+				'namedcallgroup' => '',
+				'namedpickupgroup' => '',
+				'outbound_proxy' => '',
+				'qualifyfreq' => 60,
+				'rewrite_contact' => 'yes',
+				'rtcp_mux' => 'no',
+				'rtp_symmetric' => 'yes',
+				'secret' => '0ab2282c967d042246b202ba7e199455',
+				'secret_origional' => '',
+				'sendrpid' => 'pai',
+				'sipdriver' => 'chan_sip',
+				'timers' => 'yes',
+				'transport' => '',
+				'trustrpid' => 'yes'
+			));
+
+		self::$freepbx->Core = $stubConfig;
+
+		// run the test
+		$query = "query { 
 			coreDevice(id: \"{$testExtension}\") { 
 				id,
 				device_id,
@@ -49,9 +106,29 @@ class CoreDeviceGQLTest extends ApiBaseTestCase {
 				dial,
 				emergency_cid
 			}
-		}");
+		}";
+		$response = $this->request($query);
 
+		echo "\n";
+		print_r((string)$response->getBody());
+		echo "\n";
+		
+		$token = $this->createAccessToken();
+		$response = (new Client)->request('post', 'http://localhost/admin/api/api/gql', [
+			'headers' => [
+				'Authorization' => 'Bearer ' . $token,
+				'Content-Type' => 'application/graphql'
+			],
+			'body' => $query
+		]);
+
+		echo "\n";
+		print_r((string)$response->getBody());
+		echo "\n";
+
+		/*
 		$json = json_decode((string)$response->getBody(), true);
+
 		$mutationId = base64_encode("coredevice:{$testExtension}");
 		$this->assertEquals($json, array(
 			'data' => array(
@@ -65,5 +142,6 @@ class CoreDeviceGQLTest extends ApiBaseTestCase {
 				)
 			)
 		));
+		*/
 	}
 }
